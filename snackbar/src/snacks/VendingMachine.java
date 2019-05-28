832ffcf77409ffe4936d8319598177fde0cffbd6
Roger Caballero <rogercp1234@yahoo.com>
2019-05-27 17:48:18 -0700

    added vending machine methods

--- snackbar/src/snacks/VendingMachine.java
+++ snackbar/src/snacks/VendingMachine.java
@@ -13,4 +13,29 @@ public class VendingMachine
 		id=MaxId;
 		this.name=name;
 	}
+
+	public int getId()
+	{
+		return id;
+	}
+
+	public String setname(String name)
+	{
+		this.name=name;
+	}
+
+	public String getName()
+	{
+		return this.name;
+	}
+
+	@Override 
+	public String toString()
+	{
+		String rtnStr="Vending Machine's id: " + id + "\n";
+						"Vending Machine's name:" + name + "\n";
+
+				return rtnStr;
+	}
+
 }
